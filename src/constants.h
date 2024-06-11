c5a7f0afe284ac8338c6dbe372abddb2af8808b1
wojtess <49391462+wojtess@users.noreply.github.com>
2024-06-11 22:39:31 +0200

    changed constants

--- src/constants.h
+++ src/constants.h
@@ -1,15 +1,15 @@
 #pragma once
 const int STACK_SIZE = 6;
-const int ZOMBIE_COUNT = 00;
+const int ZOMBIE_COUNT = 100;
 const float ZOMBIE_HP = 10.0f;
 const float PLAYER_HP = 100.0f;
-const float ZOMBIE_DAMAGE = 10.0f;
+const float ZOMBIE_DAMAGE = 5.0f;
 const float ZOMBIE_SIGHT = 1500.0f;
-const float ZOMBIE_ATTACK_DISTNACE = 50.0f;
-const float ZOOM = 1.0f;
+const float ZOMBIE_ATTACK_DISTNACE = 25.0f;
+const float ZOOM = 2.0f;
 const float MEDKIT_CHANCE = 0.1f;
 const float ZOMBIE_MAX_SPEED = 2000.f;
-const float ZOMBIE_MIN_SPEED = 1800.f;
+const float ZOMBIE_MIN_SPEED = 800.f;
 const float MIN_X = -700;
 const float MIN_Y = -700;
 const float MAX_X = 1200;
